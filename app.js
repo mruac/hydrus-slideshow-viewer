40a092f1a0f62f5224e71529173e133b9ffbd33f
mruac <ant8672@gmail.com>
2023-01-30 09:31:21 +1030

    switched to nodejs

---
+++ app.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', function(req, res, next) {
+  res.render('index', { title: 'Hydrus Slideshow Viewer' });
+});
+
+app.listen(3415, () => console.log(`Server listening on port: 3415`));
